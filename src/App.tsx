28f288a0ffd2faea9caaec4e2d599d6fe5f4a5b8
Olanrewaju taiwo <107349861+kiddo4@users.noreply.github.com>
2024-02-23 13:36:34 +0100

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div>
       {alertVisible && (
-        <Alert>
+        <Alert onClose={() => setAlertVisibility(false)}>
           Hello <span>World</span>
         </Alert>
       )}
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -2,9 +2,21 @@ import { ReactNode } from "react";
 
 interface props {
   children: ReactNode;
+  onClose: () => void;
 }
-const Alert = ({ children }: props) => {
-  return <div className="alert alert-primary">{children}</div>;
+const Alert = ({ children, onClose }: props) => {
+  return (
+    <div className="alert alert-primary alert-dismissible">
+      {children}
+      <button
+        type="button"
+        className="btn-close"
+        onClick={() => onClose}
+        data-bs-dismiss="alert"
+        aria-label="Close"
+      ></button>
+    </div>
+  );
 };
 
 export default Alert;
